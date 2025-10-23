1e9b67597fbad91cd7965bf96716a1b51498cfb4
aihuachun <aihuachun22@mails.ucas.ac.cn>
2025-10-23 10:50:47 +0800

    modify data type

--- src/test.py
+++ src/test.py
@@ -2,8 +2,8 @@ import cdma_mover
 import numpy as np
 cdma_mover.say_hello()
 
-a = np.arange(0, 10, 1, dtype=np.int32)
-b = np.zeros(10, dtype=np.int32)
+a = np.arange(0, 10, 1, dtype=np.int8)
+b = np.zeros(10, dtype=np.int8)
 print(f"Before move: a = {a}, b = {b}")
-cdma_mover.data_mover("0000:00:04.0",a.ctypes.data, b.ctypes.data, 10 * 4) # 10 elements * 4 bytes each (int32)
+cdma_mover.data_mover("0000:00:04.0",a.ctypes.data, b.ctypes.data, 10 * 1) # 10 elements * 4 bytes each (int32)
 print(f"After move: a = {a}, b = {b}")
